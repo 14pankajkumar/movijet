bd873ed7f015e091212c92d7d45e7807dff35263
Pankaj Kumar <pankajkumardas727@gmail.com>
2022-02-04 03:48:11 +0530

    initial commit_2

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,25 +1,30 @@
-import { useRouter } from "next/router";
+import { useRouter } from 'next/router'
+import { Genres } from '../utilities/typing'
 
-function Nav({ genres }) {
-  const router = useRouter();
+interface Props {
+  genres: [Genres]
+}
+
+function Nav({ genres }: Props) {
+  const router = useRouter()
   return (
     <nav className="relative">
-      <div className="flex py-1 px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+      <div className="flex space-x-10 overflow-x-scroll whitespace-nowrap py-1 px-10 text-2xl scrollbar-hide sm:space-x-20 sm:px-20">
         {genres.map((item) => {
           return (
             <h2
               key={item.id}
               onClick={() => router.push(`/genre/${item.id}`)}
-              className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+              className="transform cursor-pointer transition duration-100 last:pr-24 hover:scale-125 hover:text-white active:text-red-500"
             >
               {item.name}
             </h2>
-          );
+          )
         })}
       </div>
-      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"></div>
+      <div className="absolute top-0 right-0 h-10 w-1/12 bg-gradient-to-l from-[#06202A]"></div>
     </nav>
-  );
+  )
 }
 
-export default Nav;
+export default Nav
