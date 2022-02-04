2a4368671d3858af0edbd0182a8fe707bf2c31c9
Pankaj Kumar <pankajkumardas727@gmail.com>
2022-02-05 01:53:40 +0530

    added GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -11,6 +12,21 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
       </Head>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-SQVCR9TMP8"
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+               gtag('js', new Date());
+            
+               gtag('config', '${process.env.NEXT_PUBLIC_GA_KEY}');
+          `}
+      </Script>
       <Component {...pageProps} />
     </>
   )
