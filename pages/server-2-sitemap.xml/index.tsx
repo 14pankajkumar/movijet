8fdcfb26c28cc18f3633f58d521fec9e5f81bcc6
Pankaj Kumar <pankajkumardas727@gmail.com>
2022-02-08 02:58:20 +0530

    sitemap-3

--- pages/server-2-sitemap.xml/index.tsx
+++ pages/server-2-sitemap.xml/index.tsx
@@ -1,11 +1,15 @@
 import axios from 'axios'
 import { GetServerSideProps } from 'next'
 import { getServerSideSitemap, ISitemapField } from 'next-sitemap'
-import { Results } from '../../utilities/typing'
+import { Results, Genres } from '../../utilities/typing'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const API_KEY = process.env.API_KEY
   const BASE_URL = process.env.BASE_URL
+
+  const genreData = await axios
+    .get(`${BASE_URL}/3/genre/movie/list?api_key=${API_KEY}&language=en-US`)
+    .then((res) => res.data.genres)
 
   const discoverData = await axios
     .get(
@@ -13,12 +17,10 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     )
     .then((res) => res.data.results)
 
-  console.log(discoverData)
-
   const fields: ISitemapField[] = discoverData.map((item: Results) => ({
     loc: `https://movijet.vercel.app/media/${item.id}?title=${item.title
       .toLowerCase()
-      .replaceAll(' ', '-')}`,
+      .replace(' ', '-')}`,
     lastmod: new Date().toISOString(),
   }))
 
