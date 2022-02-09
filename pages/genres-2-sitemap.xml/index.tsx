dfc6507d10cfe2aeff9fa1cbd1cb3b6dbb3b24f2
Pankaj kumar <pankajkumardas272@gmail.com>
2022-02-09 19:10:28 +0530

    minor change_5

--- pages/genres-2-sitemap.xml/index.tsx
+++ pages/genres-2-sitemap.xml/index.tsx
@@ -1,24 +1,22 @@
 import axios from 'axios'
 import { GetServerSideProps } from 'next'
 import { getServerSideSitemap, ISitemapField } from 'next-sitemap'
-import { Results } from '../../utilities/typing'
+import { Genres } from '../../utilities/typing'
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const API_KEY = process.env.API_KEY
   const BASE_URL = process.env.BASE_URL
 
-  const discoverData = await axios
-    .get(
-      `${BASE_URL}/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
-    )
-    .then((res) => res.data.results)
-  
-  const fields: ISitemapField[] = discoverData.map((item: Results) => ({
-    loc: `https://movijet.vercel.app/media/${item.id}?title=${item.title.toLowerCase().replaceAll(' ', '-')}`,
+  const genreData = await axios
+    .get(`${BASE_URL}/3/genre/movie/list?api_key=${API_KEY}&language=en-US`)
+    .then((res) => res.data.genres)
+
+  const fields: ISitemapField[] = genreData.map((item: Genres) => ({
+    loc: `https://movijet.vercel.app/genre/${item.id}?=${item.name}`,
     lastmod: new Date().toISOString(),
   }))
 
   return getServerSideSitemap(ctx, fields)
 }
 
-export default function Action() {}
+export default function Site() {}
