3cebbaec016ae3e970975adeb2653ca2532f15cd
Pankaj kumar <pankajkumardas272@gmail.com>
2022-02-09 19:14:43 +0530

    minor change_6

--- pages/genres-2-sitemap.xml/index.tsx
+++ pages/genres-2-sitemap.xml/index.tsx
@@ -7,11 +7,13 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const API_KEY = process.env.API_KEY
   const BASE_URL = process.env.BASE_URL
 
-  const genreData = await axios
-    .get(`${BASE_URL}/3/genre/movie/list?api_key=${API_KEY}&language=en-US`)
-    .then((res) => res.data.genres)
+  const discoverData = await axios
+    .get(
+      `${BASE_URL}/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+    )
+    .then((res) => res.data.results)
 
-  const fields: ISitemapField[] = genreData.map((item: Genres) => ({
+  const fields: ISitemapField[] = discoverData.map((item: Genres) => ({
     loc: `https://movijet.vercel.app/genre/${item.id}?=${item.name}`,
     lastmod: new Date().toISOString(),
   }))
