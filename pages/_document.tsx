191e494158974991051ebf08f1aff2c55a57b5a7
Pankaj kumar <pankajkumardas272@gmail.com>
2022-02-13 17:59:08 +0530

    added ads

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,29 +34,18 @@ class Document extends NextDocument<Props> {
             rel="stylesheet"
           />
           {/* adsterra */}
-          <script
-            type="text/javascript"
-            src="//pl17017879.trustedcpmrevenue.com/50/ce/f1/50cef132aa49ea6969a9f7c941450832.js"
-          ></script>
+          {/* Native banner */}
           <script
             async={true}
             data-cfasync="false"
             src="//pl17018648.trustedcpmrevenue.com/468f7fa593d3c46fb949e12b0a034066/invoke.js"
           ></script>
+
+          {/* social bar */}
           <script
             type="text/javascript"
-            src="//pl17018699.trustedcpmrevenue.com/38/aa/35/38aa3582b590fd76b5cd01b4c7ed32cd.js"
+            src="//pl17017879.trustedcpmrevenue.com/50/ce/f1/50cef132aa49ea6969a9f7c941450832.js"
           ></script>
-          <script type="text/javascript">
-            {`atOptions = {
-              'key' : 'fefa90b006d9a61a726072223dfa2ef2',
-              'format' : 'iframe',
-              'height' : 60,
-              'width' : 468,
-              'params' : {}
-            };
-            document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivedisplaycontent.com/fefa90b006d9a61a726072223dfa2ef2/invoke.js"></scr' + 'ipt>');`}
-          </script>
         </Head>
 
         <body>
