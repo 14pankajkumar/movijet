29d0ff836e587503bcba79dc9ab6a269fbe48387
Pankaj kumar <pankajkumardas272@gmail.com>
2022-02-13 18:21:54 +0530

    changed description

--- pages/media/[id].tsx
+++ pages/media/[id].tsx
@@ -61,15 +61,14 @@ const Media = ({
         />
         <meta
           name="description"
-          content="Movijet where you can get the latest information about your favourite movies and
-          TV shows. Entertainment 720p Movies, 1080p movies, Dual Audio Movies, Hindi Dubbed Series, Hollywood Movies"
+          content={mediaUrl?.title || media.title}
         />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <meta httpEquiv="X-UA-Compatible" content="IE=7" />
         <meta
           name="keywords"
-          content={`${media.title}, movies to watch on netflix, movies to watch on prime, Movies, TV Shows, Streaming, Reviews, API, Actors, Actresses, Photos, User Ratings, Synopsis, Trailers, Teasers, Credits, Cast`}
+          content={`${media.title}, movies to watch on netflix, movies to watch on prime, Movies, TV Shows, Streaming, Reviews, Actors, Actresses, Photos, User Ratings, Synopsis, Trailers, Teasers, Credits, Cast`}
         />
       </Head>
 
